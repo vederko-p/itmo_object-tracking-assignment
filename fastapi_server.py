cac0296d13c9a69bee53628f96550d0ba9e4111c
Nick Sidelnikov <nikita04042000@gmail.com>
2023-04-26 10:29:25 +0300

    Update fastapi_server.py

--- fastapi_server.py
+++ fastapi_server.py
@@ -9,6 +9,29 @@ country_balls = [{'cb_id': x, 'img': imgs[x % len(imgs)]} for x in range(country
 print('Started')
 
 
+def id_generator():
+    current = 0
+    while True:
+        yield current
+        current += 1
+
+def iou(bbox1, bbox2):
+    xA = max(bbox1[0], bbox2[0])
+    yA = max(bbox1[1], bbox2[1])
+    xB = min(bbox1[2], bbox2[2])
+    yB = min(bbox1[3], bbox2[3])
+
+    inter_area = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+
+    bbox1_area = (bbox1[2] - bbox1[0] + 1) * (bbox1[3] - bbox1[1] + 1)
+    bbox2_area = (bbox2[2] - bbox2[0] + 1) * (bbox2[3] - bbox2[1] + 1)
+
+    return inter_area / float(bbox1_area + bbox2_area - inter_area)
+
+#variables for tracker_soft
+id_gen = id_generator()
+prev = []
+
 def tracker_soft(el):
     """
     Необходимо изменить у каждого словаря в списке значение поля 'track_id' так,
@@ -24,6 +47,40 @@ def tracker_soft(el):
     вашего трекера, использовать его в алгоритме трекера запрещено
     - запрещается присваивать один и тот же track_id разным объектам на одном фрейме
     """
+    global prev
+
+    if not prev:
+        for det in el['data']:
+            if det['track_id'] is None and len(det['bounding_box']) != 0:
+                det['track_id'] = next(id_gen)
+                prev.append(det)
+    else:
+        for det in el['data']:
+            if len(det['bounding_box']) != 0:
+                max_iou = 0
+                iou_id = None
+                for i, prev_det in enumerate(prev):
+                    cur_iou = iou(det['bounding_box'], prev_det['bounding_box'])
+                    if cur_iou > max_iou:
+                        max_iou = cur_iou
+                        iou_id = prev_det['track_id']
+                        idx = i
+                if max_iou != 0:
+                    det['track_id'] = iou_id
+                    prev.pop(idx)
+                else:
+                    det['track_id'] = next(id_gen)
+            else:
+                det['track_id'] = None
+
+        prev = []
+        for det in el['data']:
+           if len(det['bounding_box']) != 0:
+               prev.append(det)
+
+        for det in prev:
+            print(det)
+
     return el
 
 
